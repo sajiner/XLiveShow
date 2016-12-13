3f995061dd83cb4e286ff0984803c601067db57c
sajiner <739134362@qq.com>
2016-12-13 11:29:40 +0800

    搭建基础界面

---
+++ XLiveShow/Section/Main/XNavigationController.swift
@@ -0,0 +1,21 @@
+//
+//  XNavigationController.swift
+//  XLiveShow
+//
+//  Created by sajiner on 2016/12/13.
+//  Copyright © 2016年 sajiner. All rights reserved.
+//
+
+import UIKit
+
+class XNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+}
---
+++ XLiveShow/Section/Main/XTabBarController.swift
@@ -0,0 +1,28 @@
+//
+//  XTabBarController.swift
+//  XLiveShow
+//
+//  Created by sajiner on 2016/12/13.
+//  Copyright © 2016年 sajiner. All rights reserved.
+//
+
+import UIKit
+
+class XTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        addChildVc("XLiveViewController")
+        addChildVc("XRankViewController")
+        addChildVc("XFindViewController")
+        addChildVc("XMineViewController")
+    }
+}
+
+extension XTabBarController {
+    func addChildVc(_ storyboardName: String) {
+        guard let childVc = UIStoryboard(name: storyboardName, bundle: nil).instantiateInitialViewController() else { return }
+        addChildViewController(childVc)
+    }
+}
